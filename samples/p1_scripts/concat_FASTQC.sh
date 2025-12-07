c469f3b703419f5bb185ef866cb0c0169edabf97
gt25963 <gt25963@bristol.ac.uk>
2025-12-07 19:41:03 +0000

    Update concat_FASTQC.sh

--- samples/p1_scripts/concat_FASTQC.sh
+++ samples/p1_scripts/concat_FASTQC.sh
@@ -7,17 +7,17 @@ cat sampleA_part1.FASTQ sampleA_part2.FASTQ sampleA_part3.FASTQ > sampleA.fastq
 echo "sampleA.fastq created"
 
 #Join Sample B files
-##Concatemate the 3 parts for sample B into one complete file
+##Concatenate the 3 parts for sample B into one complete file
 cat sampleB_part1.FASTQ sampleB_part2.FASTQ sampleB_part3.FASTQ > sampleB.fastq
 echo "sampleB.fastq created"
 
 #Join Sample C files
-##Concatemate the 3 parts for sample C into one complete file
+##Concatenate the 3 parts for sample C into one complete file
 cat sampleC_part1.FASTQ sampleC_part2.FASTQ sampleC_part3.FASTQ > sampleC.fastq
 echo "sampleC.fastq created"
 
 #Join Sample D files
-##Concatemate the 3 parts for sample D into one complete file
+##Concatenate the 3 parts for sample D into one complete file
 cat sampleD_part1.FASTQ sampleD_part2.FASTQ sampleD_part3.FASTQ > sampleD.fastq
 echo "sampleD.fastq created"
 
